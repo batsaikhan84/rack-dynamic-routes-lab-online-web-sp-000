59c7cff6f68b14e5a9e07bff84eb39e212784f9b
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-29 00:25:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.title == item_name}
       resp.write item.price
     else
       resp.write "Route not found"
