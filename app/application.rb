75b65fb32d1f5406b6e638694d0457f1eaaeb6f1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-29 00:09:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = 
+      item = object.find{|s| s.title = item_name}
+      resp.write item.
     
 end
