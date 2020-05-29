84baf5a25e8124d4784f08116ef06ddceaf6f52a
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-29 00:40:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
         item_name = req.path.split("/items/").last
-        item = @@item.find{|i| i.title == item_name}
+        item = @@item.find{|i| i.name == item_name}
         if !item.nil?
           resp.write item.price
         else
